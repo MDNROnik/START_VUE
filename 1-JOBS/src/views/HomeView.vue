6dc61e5ba6424ccff30c504a503ebcfde7d5cbf2
MDNROnik <nimurrahnananik@gmail.com>
2025-03-16 15:58:32 +0600

    Changes

--- 1-JOBS/src/views/HomeView.vue
+++ 1-JOBS/src/views/HomeView.vue
@@ -4,7 +4,7 @@ import HomeCards from "../components/HomeCards.vue";
 import Titles from "../components/Titles.vue";
 </script>
 <template>
-    <Titles title="TEST PROPS" subTitle="Add Subtitles" />
+    <Titles title="Are Your A Vue Developer ?" subTitle="Discover which Vue jobs suit your needs and skill set." />
     <HomeCards />
     <JobListing limit="3"/>
 </template>
