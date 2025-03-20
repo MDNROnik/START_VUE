a70e73038a4c66bdeb562ad8cf864407907370bc
MDNROnik <nimurrahnananik@gmail.com>
2025-03-20 23:57:18 +0600

    Logics are setup

--- 2-Reaction-Timer/src/App.vue
+++ 2-Reaction-Timer/src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
+import { ref } from "vue";
+import Block from "./components/Block.vue";
+
+let play = ref(false);
+let delay = ref(null);
+const playTheGame = () => {
+  play.value = true;
+  delay.value = 2000 + Math.random() * 5000;
+  console.log("delay", delay.value);
+};
+
+const reSetTheButton = () => {
+  console.log("resetting the button from block");
+  
+  play.value = false;
+  delay.value = null;
+};
 </script>
 
 <template>
-  <h1>START A BASIC PROJECT FOR LEARNING</h1>
+  <div class="flex items-center justify-center">
+    <div>
+      <h1>START A BASIC PROJECT FOR LEARNING</h1>
+      <button @click="playTheGame"  :disabled="play">
+        <div v-if="play==false" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">PLAY NOW</div>
+        <div v-else class="bg-blue-200 text-white font-bold py-2 px-4 rounded-full">PLAY NOW</div>
+        <!-- <div>PLAY NOW</div> -->
+      </button>
+      <Block class="box-border size-50 border-4 p-4" v-if="play==true" :delay="delay" :appFunction="reSetTheButton" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ 2-Reaction-Timer/src/components/Block.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const props = defineProps (
+    {
+        delay: Number,
+        play : Boolean,
+        appFunction: Function,
+    }
+)
+
+let showBlock = ref(false);
+
+
+onMounted(() => {
+    console.log(`delay in block: ${props.delay}`);
+    
+    setTimeout(() => {
+        showBlock.value = true;
+    }, props.delay);
+});
+const fun=()=>{
+    showBlock.value=false;
+    console.log("clicked");
+    props.appFunction();
+}
+
+</script>
+
+<template>
+    <div v-if="showBlock==true" @click="fun">
+        <h1>hello world {{ delay }}</h1>
+    </div>
+</template>
