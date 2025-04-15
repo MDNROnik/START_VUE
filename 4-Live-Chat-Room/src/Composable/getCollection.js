97b50e9b9ce6c9e3ae9becf1d0d5453edd938018
MDNROnik <nimurrahnananik@gmail.com>
2025-04-15 11:34:09 +0600

    Set Up Message Box

--- 4-Live-Chat-Room/src/Composable/getCollection.js
+++ 4-Live-Chat-Room/src/Composable/getCollection.js
@@ -1,10 +1,11 @@
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import { onUnmounted , ref } from 'vue';
 import { db } from '../firebase/configs.js';
 
 const getCollection = (collectionName) => {
     const documents = ref(null);
     const error = ref(null); 
+    
 
     // let collectionRef  = collection(db, collectionName);
     // collectionRef.onSnapshot(
@@ -22,7 +23,10 @@ const getCollection = (collectionName) => {
     //     error.value = 'Could not fetch the data';
     // }
     // );
-    const q = query(collection(db, collectionName));
+
+    // const q = query(collection(db, collectionName));
+
+    const q = query(collection(db, collectionName), orderBy('createdAt'));
     // console.log(collectionName);
     // console.log(q);
     const unsubscribe = onSnapshot(q, (snapshot) => { 
@@ -31,12 +35,9 @@ const getCollection = (collectionName) => {
         
         snapshot.docs.forEach(doc => {
             doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
-            // console.log(doc.data().name);
-            // console.log(doc.data().message);
-            // console.log(doc.data().createdAt);
         });
         documents.value = results;
-        console.log(results);
+        // console.log(results);
         error.value = null; 
     }, (err) => {
         console.log(err.message);
--- 4-Live-Chat-Room/src/components/ChatsWindow.vue
+++ 4-Live-Chat-Room/src/components/ChatsWindow.vue
@@ -1,33 +1,66 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-else-if="documents.length === 0">No messages yet</div>
-    <div v-else>
-        <div v-for="doc in documents" :key="doc.id" class="single">
-          <span class="created-at">
-            {{ doc.createdAt }}
-          </span>
-          <span class="name">
-            {{ doc.name }}
-          </span>
-          <span class="message">
-            {{ doc.message }}
-          </span>
-        </div>
+    <div v-if="formattedDocuments" ref="messages" class="messages">
+      <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+        <span class="created-at">{{ doc.createdAt }}</span>
+        <span class="name">{{ doc.name }}</span>
+        <span class="message">{{ doc.message }}</span>
+      </div>
     </div>
-    <h1>HELLO WORLD</h1>
   </div>
 </template>
 
 <script setup>
-import getCollection from '@/Composable/getCollection.js';
+import getCollection from "@/Composable/getCollection.js";
+import { formatDistanceToNow } from "date-fns";
+import { computed, onUpdated, ref, watchEffect } from "vue";
 
-const { documents, error } = getCollection ('messages');
+const { documents, error } = getCollection("messages");
 console.log("documents: ", documents);
+watchEffect(() => {
+  if (documents.value) {
+    console.log("Documents loaded:", documents.value);
+  }
+});
+// format timestamp
+const formattedDocuments = computed(() => {
+  if (documents.value) {
+    return documents.value.map((doc) => {
+      let time = formatDistanceToNow(doc.createdAt.toDate());
+      return { ...doc, createdAt: time };
+    });
+  }
+});
 
 
+// auto-scroll to bottom of messages
+const messages = ref(null);
+onUpdated(() => {
+  messages.value.scrollTop = messages.value.scrollHeight;
+});
 </script>
 
-<style>
-
+<style scoped>
+.chat-window {
+  background: #fafafa;
+  padding: 30px 20px;
+}
+.single {
+  margin: 18px 0;
+}
+.created-at {
+  display: block;
+  color: #999;
+  font-size: 12px;
+  margin-bottom: 4px;
+}
+.name {
+  font-weight: bold;
+  margin-right: 6px;
+}
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}
 </style>
