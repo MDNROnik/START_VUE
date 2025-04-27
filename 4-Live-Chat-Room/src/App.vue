053be8e2942ed0efb53af7229196de2a140eb3ca
MDNROnik <nimurrahnananik@gmail.com>
2025-04-27 17:05:10 +0600

    design changes

--- 4-Live-Chat-Room/src/App.vue
+++ 4-Live-Chat-Room/src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <!-- <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/chat"> Chat Room</RouterLink> -->
-  <RouterView />
+  <div class="front">
+    <div  style="text-align: center;">
+      <h1>Chat Room</h1>
+    </div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
 
+.front{
+  height: 180px;
+}
+
+h1 {
+  font-size: 3rem;
+  color: #333;
+}
 </style>
